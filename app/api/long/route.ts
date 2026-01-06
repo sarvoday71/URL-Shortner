c6fb71a9e97d41af3706c08b1c74aead98072995
sarvoday71 <sarvodayjadhav17@gmail.com>
2026-01-06 16:05:15 +0530

    changed the prisma schema to have an output in node modules

--- app/api/long/route.ts
+++ app/api/long/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../lib/prisma";
-export const runtime = 'nodejs';
 
 export async function POST(request: NextRequest) {
     try {
