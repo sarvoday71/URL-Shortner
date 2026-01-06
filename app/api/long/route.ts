f50c8d8754feb281f622b6d392632c33b22aa81f
sarvoday71 <sarvodayjadhav17@gmail.com>
2026-01-06 14:55:38 +0530

    Used find unique insteand of find first

--- app/api/long/route.ts
+++ app/api/long/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest) {
             return NextResponse.json({ error: "longUrl missing" }, { status: 400 });
         }
         console.log(longUrl);
-        let shortUrl = await prisma.urlDB.findFirst({
+        let shortUrl = await prisma.urlDB.findUnique({
             where: {
                 longurl: longUrl
             }
